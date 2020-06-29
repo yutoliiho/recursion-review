4517ce7cad8e9dde605468616e81eaf5db04a0bc
Tong Yu <yutongsf@gmail.com>
2020-06-29 16:24:59 -0700

    testing susan github

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -5,14 +5,13 @@ var htmlStrings = [
   '<div><div class="targetClassName"><div class="targetClassName"></div></div></div>',
   '<div><div></div><div><div class="targetClassName"></div></div></div>',
   '<div><div class="targetClassName"></div><div class="targetClassName"></div></div>',
-  '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>'
+  '<div><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></div>',
 ];
 
-describe('getElementsByClassName', function() {
-
-  it('should match the results of calling the built-in function', function() {
+describe('getElementsByClassName', function () {
+  it('should match the results of calling the built-in function', function () {
     $('body').addClass('targetClassName');
-    htmlStrings.forEach(function(htmlString) {
+    htmlStrings.forEach(function (htmlString) {
       var $rootElement = $(htmlString);
       $('body').append($rootElement);
 
@@ -20,11 +19,10 @@ describe('getElementsByClassName', function() {
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
-      expect(equality).to.equal(FILL_ME_IN);
+      expect(equality).to.equal(true);
 
       $rootElement.remove();
     });
     $('body').removeClass('targetClassName');
   });
-
 });
